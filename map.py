fa46bc57a8901f490050392f816ae9fded6f581d
AyadAbdullah <158589968+AyadAbdullah@users.noreply.github.com>
2024-06-01 17:16:05 +0000

    started the overall tile
    Each tile will have its own map.

---
+++ map.py
@@ -0,0 +1,7 @@
+from tabulate import tabulate
+
+overall_map_room = [
+    ["Hell's Kitchen Docks", "Gang Hideout", "Hell's Kitchen Downtown",
+    "Alleyway"],
+["New York Bulletin Building", "Prison", "Wilson Fisk's Penthouse"]
+]  #Alleyway& Wilson Fisk's Penthouse is boss fight
